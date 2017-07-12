e82f52e856af7a68a56ccd56d17cd1e473ad6572
Chris Co <ufcordle@hotmail.com>
2017-07-11 20:03:39 -0400

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+const something = 'Yolo';
+
+const Index = () => (
+  <div>
+    <h2>Chris Cordle</h2>
+    <p>Hello Chris</p>
+    <p>It's like I'm blah in 1997 all over again!!</p>
+    <p>{ something }</p>
+  </div>
+)
+
+export default Index
